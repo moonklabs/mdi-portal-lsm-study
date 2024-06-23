33acf35f2fbbebe0ae21242fabf177b239e0aa05
waterfall <cvb575859@gmail.com>
2024-06-24 02:03:50 +0900

    refactor: draggable -> rnd 드래그앤드랍, 리사이즈 라이브러리 변경

--- mdi-react/src/compnents/panel/Panel.jsx
+++ mdi-react/src/compnents/panel/Panel.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import Draggable from 'react-draggable';
+import { Rnd } from 'react-rnd';
 import { Paper, Box } from '@mui/material';
 import { bringToFront, updatePanel } from '../../features/panels/panelSlice';
 import PanelHeader from './PanelHeader';
@@ -12,39 +12,45 @@ const Panel = ({ panel }) => {
   const panelRef = useRef(null);
   const currentTime = useClock(panel.timezone);
 
-  const handleDragStart = () => {
-    dispatch(
-      updatePanel({
-        ...panel,
-        isMaximized: false,
-      })
-    );
-  };
-
   const handleDragStop = (e, data) => {
     dispatch(updatePanel({ ...panel, x: data.x, y: data.y }));
   };
 
+  const handleResizeStop = (e, direction, ref, delta, position) => {
+    const width = parseFloat(ref.style.width);
+    const height = parseFloat(ref.style.height);
+    if (!isNaN(width) && !isNaN(height)) {
+      dispatch(
+        updatePanel({
+          ...panel,
+          width,
+          height,
+          x: position.x,
+          y: position.y,
+        })
+      );
+    }
+  };
   const handleClick = () => {
     dispatch(bringToFront(panel.id));
   };
 
   return (
-    <Draggable
-      nodeRef={panelRef}
-      handle=".panel-header"
+    <Rnd
+      size={{ width: panel.width, height: panel.height }}
       position={{ x: panel.x, y: panel.y }}
-      onStart={handleDragStart}
-      onStop={handleDragStop}
+      onDragStop={handleDragStop}
+      onResizeStop={handleResizeStop}
       onMouseDown={handleClick}
+      minWidth={200}
+      minHeight={200}
+      bounds="parent"
     >
       <Box
         ref={panelRef}
         sx={{
-          position: 'absolute',
-          zIndex: 1000,
-          width: panel.width,
-          height: panel.height,
+          width: '100%',
+          height: '100%',
         }}
       >
         <Paper
@@ -61,7 +67,7 @@ const Panel = ({ panel }) => {
           <PanelContent {...panel} currentTime={currentTime} />
         </Paper>
       </Box>
-    </Draggable>
+    </Rnd>
   );
 };
 
